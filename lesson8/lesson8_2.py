0360e48a5aced484a46b35ee2d906587e0a5f4d8
徐國堂 <roberthsu2003@gmail.com>
2024-08-21 12:19:09 +0000

    modify

--- lesson8/lesson8_2.py
+++ lesson8/lesson8_2.py
@@ -1,11 +1,12 @@
-import widget
+from widget import tools
+
 while True:
     try:
         name = input("請輸入姓名: ")
         height = float(input('請輸入身高（cm）: '))
         weight = float(input('請輸入體重（kg）: '))
         bmi = weight / ((height * 0.01) ** 2)
-        grade = widget.tools.get_status_message(bmi)
+        grade = tools.get_status_message(bmi)
         print(f"{name} 的 BMI 為 {bmi:.2f}, 為 {grade}")
     except ValueError:
         print("格式錯誤，請重新輸入數據")
