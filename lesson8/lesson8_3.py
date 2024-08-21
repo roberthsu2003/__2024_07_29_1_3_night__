e0b2a97a73f144f43c909d553e5a5af16a5b69ff
徐國堂 <roberthsu2003@gmail.com>
2024-08-21 12:39:24 +0000

    modify

--- lesson8/lesson8_3.py
+++ lesson8/lesson8_3.py
@@ -1,24 +1,4 @@
-class MyClass:
-    @classmethod
-    def get_status_message(cls,bmi: float) -> str:
-        '''
-        #param bmi:這是要傳入bmi傳
-        #return:傳出bmi的狀態
-        '''
-
-        if bmi < 18.5:
-            return "體重過輕"
-        elif bmi < 24:
-            return "正常範圍"
-        elif bmi < 27:
-            return "過重"
-        elif bmi < 30:
-            return "輕度肥胖"
-        elif bmi < 35:
-            return "中度肥胖"
-        else:
-            return "重度肥胖"
-
+from widget import MyClass
 
 while True:
     try:
