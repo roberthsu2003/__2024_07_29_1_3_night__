b229f745ed57a0930a7c9d648999821d352c8684
徐國堂 <roberthsu2003@gmail.com>
2024-10-09 13:20:45 +0000

    修改main.py的內容

--- flask/main.py
+++ flask/main.py
@@ -4,4 +4,6 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<h1>Hello, World!</h1>"
+    return '''<h1>這是python的project</h1>
+            <p>這是在codespace環境開發的</p>
+        '''
